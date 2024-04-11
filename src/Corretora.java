2008eb04869efab58d5c1dffb95bb279eb5e8fe2
AmandaMS09 <mandanevs@gmail.com>
2024-04-11 00:09:18 -0300

    Estrutura inicial

---
+++ src/Corretora.java
@@ -0,0 +1,14 @@
+public class Corretora {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public void compra(int quant, double val, char[] corretora) {
+
+    }
+
+    public void venda(int quant, double val, char[] corretora) {
+
+    }
+}
