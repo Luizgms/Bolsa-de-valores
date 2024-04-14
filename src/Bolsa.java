7a2fb38c14e8ed8abe54145bfbc5d400c771a976
AmandaMS09 <mandanevs@gmail.com>
2024-04-14 17:28:09 -0300

    Adição de Threads

--- src/Bolsa.java
+++ src/Bolsa.java
@@ -10,11 +10,63 @@ import java.util.concurrent.Executors;
 
 public class Bolsa {
 
-    private final static String QUEUE_RECV = "BROKER";
-    private final static String[] BINDING_KEYS = { "compra.*", "venda.*" };
+    private final static String QUEUE_NAME = "BROKER";
     private static final String EXCHANGE_NAME = "topic_logs";
     private static final int NUM_THREADS = 1;
-    ConnectionFactory factory = new ConnectionFactory();
+    private static ConnectionFactory factory = new ConnectionFactory();
+
+    public static void main(String[] args) {
+        threadRecv.start();
+    }
+
+    static Thread threadRecv = new Thread(new Runnable() {
+        public void run() {
+            try {
+                System.out.println("Inicializando a bolsa...\n");
+                initFactory();
+                Channel channel = createChannel();
+                consumeQueue(channel);
+            } catch (IOException | TimeoutException e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    private static void initFactory() throws IOException, TimeoutException {
+        factory.setHost(Env.getHost());
+        factory.setUsername(Env.getUservhost());
+        factory.setPassword(Env.getPassword());
+        factory.setVirtualHost(Env.getUservhost());
+    }
+
+    private static Channel createChannel() throws IOException, TimeoutException {
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        return channel;
+    }
+
+    private static void consumeQueue(Channel channel) throws IOException, TimeoutException {
+        String[] BINDING_KEYS = { "compra.*", "venda.*"};
+
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+
+        for (String bindingKey : BINDING_KEYS) {
+            channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, bindingKey);
+        }
+
+        System.out.println(" [*] Bolsa de Valores Waiting for messages in queue BROKER");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println("\n [x] Bolsa de Valores Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+        };
+
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
+        });
+    }
+
+
 
     private ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
 
@@ -31,42 +83,10 @@ public class Bolsa {
                 
                 consumeQueue(channel);
 
-                System.out.println(" [*] Waiting for messages in channel BROKER");
+                System.out.println(" [*] Bolsa de Valores Waiting for messages in channel BROKER");
             } catch (IOException | TimeoutException e) {
                 e.printStackTrace();
             }
-        });
-    }
-
-    private void initFactory() throws IOException, TimeoutException {
-        factory.setHost(Env.getHost());
-        factory.setUsername(Env.getUservhost());
-        factory.setPassword(Env.getPassword());
-        factory.setVirtualHost(Env.getUservhost());
-    }
-
-    private Channel createChannel() throws IOException, TimeoutException {
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
-        channel.queueDeclare(QUEUE_RECV, false, false, false, null);
-        return channel;
-    }
-
-    private void consumeQueue(Channel channel) throws IOException, TimeoutException {
-        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
-
-        for (String bindingKey : BINDING_KEYS) {
-            channel.queueBind(QUEUE_RECV, EXCHANGE_NAME, bindingKey);
-        }
-
-        // Define the DeliverCallback within the method's scope
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
-            //TO-DO: adicionar link para método do livro
-            System.out.println(" [x] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
-        };
-
-        channel.basicConsume(QUEUE_RECV, true, deliverCallback, consumerTag -> {
         });
     }
 
